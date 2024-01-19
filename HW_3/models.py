85febbe79d840376e2fdde6c20f7c9ddc2ffd5e4
Squidward13 <adm.mail1997@yandex.ru>
2024-01-19 10:45:04 +0300

    Homework_4

--- HW_3/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(60), unique=True, nullable=False)
